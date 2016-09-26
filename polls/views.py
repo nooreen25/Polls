eee33d3bbf48905a71769a4ab99383fff2303d44
nooreenharoon <nooreenharoon@gmail.com>
2016-09-26 16:32:22 +0530

    changes

--- polls/views.py
+++ polls/views.py
@@ -7,7 +7,7 @@ from django.views import generic
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.template import loader
-
+from django.utils import timezone
 
 from .models import Choice,Question
 
@@ -18,7 +18,12 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class DetailView(generic.DetailView):
@@ -31,3 +36,5 @@ class ResultsView(generic.DetailView):
 
 
 def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
